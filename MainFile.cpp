43345c7da544873e7c59da560391a796f4c31249
Ethan <ethan.r.pena@gmail.com>
2023-08-31 17:35:02 -0400

    Added a variable for a string.

--- MainFile.cpp
+++ MainFile.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 // Just some starter script to get things going.
 
 int main()
 {
+	string word;
+	word = "hahaha text";
+
 	cout << "Hello world!" << endl;
+
+	return 0;
 }
