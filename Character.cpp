228b8432cb3f8e8a2c720c3c1387e8a4a355bb14
Ethan <ethan.r.pena@gmail.com>
2023-09-08 21:48:38 -0400

    Updated Characters to better handle dynamic stats and have proficiency and modifier implementation.

--- Character.cpp
+++ Character.cpp
@@ -1,14 +1,27 @@
 #include "Character.h"
 #include <string>
+#include <map>
 using namespace std;
 
+// Functions
+int standardModFunc(int scoreVal, int profVal, int level) {
+	return (
+		(floor(scoreVal / 2.0) - 5) + // ability score bonus
+		((floor((level - 1.0) / 4) + 2) * profVal) // proficiency bonus
+		);
+}
+
+// Constructors
 Character::Character() {
 	maxHealth = health = armorClass = 10;
 	level = 1;
 	name = "Generic Character";
 	description = "";
 	owner = "Nobody";
-	strength = dexterity = constitution = intelligence = wisdom = charisma = 10;
+	initAbilityScores();
+	initProficiencies();
+	initModifiers();
+	updateModifiers();
 }
 
 Character::Character(string _name, string _owner, int _maxHealth, int _armorClass, int _level) {
@@ -18,18 +31,111 @@ Character::Character(string _name, string _owner, int _maxHealth, int _armorClas
 	name = _name;
 	description = "";
 	owner = _owner;
-	strength = dexterity = constitution = intelligence = wisdom = charisma = 10;
+	initAbilityScores();
+	initProficiencies();
+	initModifiers();
+	updateModifiers();
 }
 
+// Private methods
+void Character::initAbilityScores() {
+	abilityScores["Strength"] = 10;
+	abilityScores["Dexterity"] = 10;
+	abilityScores["Constitution"] = 10;
+	abilityScores["Intelligence"] = 10;
+	abilityScores["Wisdom"] = 10;
+	abilityScores["Charisma"] = 10;
+}
+
+void Character::initProficiencies() {
+	// constant
+	proficiencies["NONE"] = 0;
+	// ability scores (just zeros for now)
+	proficiencies["Strength"] = 0;
+	proficiencies["Dexterity"] = 0;
+	proficiencies["Constitution"] = 0;
+	proficiencies["Intelligence"] = 0;
+	proficiencies["Wisdom"] = 0;
+	proficiencies["Charisma"] = 0;
+	// strength skills
+	proficiencies["Athletics"] = 0;
+	// dexterity skills
+	proficiencies["Acrobatics"] = 0;
+	proficiencies["Sleight of Hand"] = 0;
+	proficiencies["Stealth"] = 0;
+	// intelligence skills
+	proficiencies["Arcana"] = 0;
+	proficiencies["History"] = 0;
+	proficiencies["Investigation"] = 0;
+	proficiencies["Nature"] = 0;
+	proficiencies["Religion"] = 0;
+	// wisdom skills
+	proficiencies["Animal Handling"] = 0;
+	proficiencies["Insight"] = 0;
+	proficiencies["Medicine"] = 0;
+	proficiencies["Perception"] = 0;
+	proficiencies["Survival"] = 0;
+	// charisma skills
+	proficiencies["Deception"] = 0;
+	proficiencies["Intimidation"] = 0;
+	proficiencies["Performance"] = 0;
+	proficiencies["Persuasion"] = 0;
+}
+
+void Character::initModifiers() {
+	// ability scores (just zeros for now)
+	modifEqHolder["Strength"] = modifEq(standardModFunc, "Strength");
+	modifEqHolder["Dexterity"] = modifEq(standardModFunc, "Dexterity");
+	modifEqHolder["Constitution"] = modifEq(standardModFunc, "Constitution");
+	modifEqHolder["Intelligence"] = modifEq(standardModFunc, "Intelligence");
+	modifEqHolder["Wisdom"] = modifEq(standardModFunc, "Wisdom");
+	modifEqHolder["Charisma"] = modifEq(standardModFunc, "Charisma");
+	// strength skills
+	modifEqHolder["Athletics"] = modifEq(standardModFunc, "Strength");
+	// dexterity skills
+	modifEqHolder["Acrobatics"] = modifEq(standardModFunc, "Dexterity");
+	modifEqHolder["Sleight of Hand"] = modifEq(standardModFunc, "Dexterity");
+	modifEqHolder["Stealth"] = modifEq(standardModFunc, "Dexterity");
+	// intelligence skills
+	modifEqHolder["Arcana"] = modifEq(standardModFunc, "Intelligence");
+	modifEqHolder["History"] = modifEq(standardModFunc, "Intelligence");
+	modifEqHolder["Investigation"] = modifEq(standardModFunc, "Intelligence");
+	modifEqHolder["Nature"] = modifEq(standardModFunc, "Intelligence");
+	modifEqHolder["Religion"] = modifEq(standardModFunc, "Intelligence");
+	// wisdom skills
+	modifEqHolder["Animal Handling"] = modifEq(standardModFunc, "Wisdom");
+	modifEqHolder["Insight"] = modifEq(standardModFunc, "Wisdom");
+	modifEqHolder["Medicine"] = modifEq(standardModFunc, "Wisdom");
+	modifEqHolder["Perception"] = modifEq(standardModFunc, "Wisdom");
+	modifEqHolder["Survival"] = modifEq(standardModFunc, "Wisdom");
+	// charisma skills
+	modifEqHolder["Deception"] = modifEq(standardModFunc, "Charisma");
+	modifEqHolder["Intimidation"] = modifEq(standardModFunc, "Charisma");
+	modifEqHolder["Performance"] = modifEq(standardModFunc, "Charisma");
+	modifEqHolder["Persuasion"] = modifEq(standardModFunc, "Charisma");
+}
+
+
+void Character::updateModifiers() {
+	for (map<string, modifEq>::iterator it = modifEqHolder.begin(); it != modifEqHolder.end(); it++) {
+		(*it->second.funcPtr)(
+			abilityScores[it->second.scoreName], 
+			proficiencies[it->first],
+			level
+			);
+	}
+}
+
+// Public Methods
 void Character::SetHealth(int newHealth) {
-	health = newHealth;
+	health = min(max(newHealth, 0), maxHealth); // force between 0 and maxhealth (inclusive)
 }
 int Character::GetHealth() {
 	return health;
 }
 
 void Character::SetMaxHealth(int newMaxHealth) {
-	maxHealth = newMaxHealth;
+	maxHealth = max(newMaxHealth, 1); // force higher than 0
 }
 int Character::GetMaxHealth() {
 	return maxHealth;
@@ -47,45 +153,20 @@ string Character::GetName() {
 }
 
 void Character::SetStat(string statName, int newStat) {
-	if (statName == "STR") {
-		strength = newStat;
-	}
-	else if (statName == "DEX") {
-		dexterity = newStat;
-	}
-	else if (statName == "CON") {
-		constitution = newStat;
-	}
-	else if (statName == "INT") {
-		intelligence = newStat;
-	}
-	else if (statName == "WIS") {
-		wisdom = newStat;
-	}
-	else if (statName == "CHA") {
-		charisma = newStat;
-	}
+	abilityScores[statName] = max(newStat, 1);
 }
 int Character::GetStat(string statName) {
-	if (statName == "STR") {
-		return strength;
+	map<string, int>::iterator it = abilityScores.find(statName);
+	if (it == abilityScores.end()) {
+		return 0; // 0 means stat doesn't exist
 	}
-	else if (statName == "DEX") {
-		return dexterity;
+	return it->second;
+}
+bool Character::DelStat(string statName) {
+	map<string, int>::iterator it = abilityScores.find(statName);
+	if (it == abilityScores.end()) {
+		return false;
 	}
-	else if (statName == "CON") {
-		return constitution;
-	}
-	else if (statName == "INT") {
-		return intelligence;
-	}
-	else if (statName == "WIS") {
-		return wisdom;
-	}
-	else if (statName == "CHA") {
-		return charisma;
-	}
-	else {
-		return -1;
-	}
+	abilityScores.erase(it);
+	return true;
 }
--- Character.h
+++ Character.h
@@ -1,13 +1,31 @@
 #include <string>
+#include <map>
 #pragma once
 using namespace std;
 
 class Character
 {
 	private:
-		int maxHealth, health, armorClass, level;
-		int strength, dexterity, constitution, intelligence, wisdom, charisma;
-		string name, description, owner;
+		struct modifEq {
+			int (*funcPtr)(int scoreVal, int profVal, int level);
+			string scoreName;
+			modifEq(int (*_funcPtr)(int, int, int), string _scoreName)
+			{
+				funcPtr = _funcPtr; 
+				scoreName = _scoreName;
+			}
+		};
+		int maxHealth, health, armorClass, level;	// Necessary int data
+		string name, description, owner;			// Necessary string data
+		map<string, int> abilityScores;				// for any ability scores
+		map<string, int> proficiencies;				// for any proficiency scores
+		map<string, int> modifResults;				// holds the actual modifiers
+		map<string, modifEq> modifEqHolder;			// holds modifier equations ('modifEq's)
+		map<string, string> blurbs;					// for any text-specific traits
+		void initAbilityScores();
+		void initProficiencies();
+		void initModifiers();
+		void updateModifiers();
 	public:
 		Character();
 		Character(
@@ -23,7 +41,8 @@ class Character
 
 		string GetName();
 
-		void SetStat(string statName, int newStat);
+		void SetStat(string statName, int newValue); 
 		int GetStat(string statName);
+		bool DelStat(string statName);	// Returns success 
 };
 
